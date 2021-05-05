ddb696caec1f109c1fdbaf032f9b29e01dc7998c
gbnm2001 <78373993+gbnm2001@users.noreply.github.com>
2021-05-05 22:43:08 +0530

    Update Ghost.cpp
    improved chasing algorithm

--- pacman/Ghost.cpp
+++ pacman/Ghost.cpp
@@ -1,18 +1,27 @@
 #include "Ghost.h"
 using namespace std;
 
-Ghost::Ghost(vector<SDL_Rect> blocks){
+Ghost::Ghost(vector<SDL_Rect> blocks, int ID){
     //Initialize the offsets
+    id = ID;
 	Red_blocks = blocks;
-    mPosX = 20;
-    mPosY = 1020;
+    switch (id)
+    {//{{20, 1020}, {1020, 20}, {1860,20}, {1860, 1020}, {1020,1020}, {20, 20}};
+    case 0: mPosX = 1860; mPosY = 1020; break;
+    case 1: mPosX = 20; mPosY = 1020; break;
+    case 2: mPosX = 1020; mPosY = 1020; break;
+    case 3: mPosX = 1020; mPosY = 20; break;
+    case 4: mPosX = 1860; mPosY = 460; break;
+    case 5: mPosX = 1860; mPosY = 20; break;
+    
+    default:
+        mPosY = 1020; mPosX =20; break;
+    }
 	PacBox.x=mPosX;
 	PacBox.y=mPosY;
 	PacBox.w=40;
 	PacBox.h=40;
     count = 0;
-    rany = 20;
-    ranx = 1860;
     //Initialize the velocity
     mVelX = 0;
     mVelY = 0;
@@ -42,6 +51,8 @@ void Ghost::move(int pacx, int pacy)
     }*/
     int vdiff = mPosY - pacy;
     int hdiff = mPosX - pacx;
+    int ranx = ran[id][0];
+    int rany = ran[id][1];
     if(state == 1){//chasing
 	    if(abs(vdiff)>abs(hdiff)){
             if(vdiff<0){mVelY = 5; goto action;}
@@ -67,7 +78,7 @@ void Ghost::move(int pacx, int pacy)
             if(rhdif>0) {mVelX = -5; goto action;}
             else {mVelX = 5; goto action;}
         }
-        if (count == 300){state = 1; count=0;}
+        if (count == 300){state = 1; count=0; id++;}
     }
     else if( state == -2){//runaway
         if(abs(vdiff) > abs(hdiff)){
@@ -103,13 +114,14 @@ action:
 		PacBox.y = mPosY;
         mVelY = 0;
     }
-    if(mVelX==0 && mVelY ==0){
+    if(mVelX==0 && mVelY ==0 && count%50 == 0){
         count = 0;
-        state = -state;
-        if(pacx > 930){ ranx = 20;}
-        else {ranx = 1860;}
-        if(pacy > 540){rany = 20;}
-        else {rany = 1020;}
+        state = -state; //going to corner
+        id++;
+        //printf("%i,%i, %i\n",id, ran[id][0],ran[id][1]);
+    }
+    if(id == 6){
+        id =0;
     }
 }
 
